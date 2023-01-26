07eaac146196b0937ba37005e3b88bce6740efe9
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-01-26 13:54:44 +0600

    Time: 140 ms (37.82%), Space: 18.5 MB (42.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,25 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        cnt = defaultdict(str)
+        cnt = defaultdict(list)
         for string in strs:
-            cnt[string] = ''.join(sorted(string)) 
-        res = []
-        strs.sort(key = lambda k:cnt[k])
-        temp = [strs[0]]
-        prev = strs[0]
-        for string in strs[1:]:
-            if cnt[string] == cnt[prev]:
-                temp.append(string)
-            else:
-                res.append(temp)
-                temp = [string]
-                prev = string
-                
-        if temp == []:
-            return res
-        else:
-            res.append(temp)
-            return res
-            
+            cnt[tuple(sorted(string))].append(string)
+        return cnt.values()
+        
             
         
