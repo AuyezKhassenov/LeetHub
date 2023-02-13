ab4efdcfdcac94d02b78044399a0191cb6a52254
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-02-13 17:35:12 +0600

    Time: 1478 ms (52.17%), Space: 18.4 MB (29.52%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,13 +3,11 @@ class Solution:
         nums.sort()
         n = len(nums)
         res = []
-        duplicate = set()
-        for i in range(n-2):
         
-            
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             first = nums[i]
-           # if i > 0 and first == prev:
-           #     continue
             l = i + 1
             r = n - 1
             while l < r:
@@ -19,12 +17,12 @@ class Solution:
                 elif left + right < - first:
                     l += 1
                 else:
-                    if (first, left, right) not in duplicate:
-                        res.append([first, left, right])
-                        duplicate.add((first, left, right))
-                    r -= 1
-                    l += 1
+                    res.append([first, left, right])
                     
-            #prev = first
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    
+            
         return res
             
