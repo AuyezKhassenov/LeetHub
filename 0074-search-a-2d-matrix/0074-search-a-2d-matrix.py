3e5f03ecadf175a1e3ffa8a360533377f43994f5
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-02-16 17:59:49 +0600

    Time: 42 ms (85.69%), Space: 14.5 MB (35.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,29 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows = [row[0] for row in matrix] + [matrix[-1][-1]]
-        l, r = 0, len(matrix)
-        if target < rows[0] or target > rows[-1]:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top+bot)//2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        
+        if top > bot:
             return False
-        while l <= r and r - l > 1:
-            mid = (l + r)//2
-            if rows[mid] < target:
-                l = mid
-            elif rows[mid] > target:
-                r = mid
+        
+        left, right = 0, COLS - 1
+        while left <= right:
+            m = (left+right)//2
+            if target > matrix[row][m]:
+                left = m + 1
+            elif target < matrix[row][m]:
+                right = m - 1
             else:
                 return True
-        if target > matrix[l][-1]:
-            return False
-        else:
-            left, right = 0, len(matrix[l]) - 1
-            while left <= right and right - left > 1:
-                mid = (left + right) // 2
-                if target > matrix[l][mid]:
-                    left = mid
-                elif target < matrix[l][mid]:
-                    right = mid
-                else:
-                    return True
             
-        return matrix[l][left] == target or matrix[l][right] == target
+        return False
         
