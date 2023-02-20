54ac52b3d0573a30feb96d937b63bb87dac5d74a
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-02-20 17:35:25 +0600

    Time: 762 ms (74.09%), Space: 72.4 MB (13.59%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,35 +1,33 @@
 class TimeMap:
 
     def __init__(self):
-        self.key_val = defaultdict()
         self.key_time = defaultdict(list)
         
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.key_val[(key, timestamp)] = value
-        self.key_time[key].append(timestamp)
+        self.key_time[key].append([value, timestamp])
         
     def get(self, key: str, timestamp: int) -> str:
         tmp = self.key_time[key]
-        if len(tmp) == 0 or timestamp < tmp[0]:
+        if len(tmp) == 0 or timestamp < tmp[0][1]:
             return ""
+        
         l, r = 0, len(tmp) - 1
        
-        
         while l <= r:
             m = (l+r) // 2                
             
-            if tmp[m] < timestamp:
+            if tmp[m][1] < timestamp:
                 l = m + 1
-            elif tmp[m] > timestamp:
+            elif tmp[m][1] > timestamp:
                 r = m - 1
             else:
-                return self.key_val[(key, tmp[m])]
+                return tmp[m][0]
         
-        if timestamp > tmp[m]:
+        if timestamp > tmp[m][1]:
             ind = min(len(tmp)-1,m+1)
         else:
             ind = max(0, m-1)
-        return self.key_val[(key, tmp[ind])] 
+        return tmp[ind][0]
         
 
 
