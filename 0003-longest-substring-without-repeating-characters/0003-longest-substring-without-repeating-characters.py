be41a6c905700d1572ce708630c0877376a52bd9
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-02-21 21:25:51 +0600

    Time: 115 ms (30.16%), Space: 14 MB (90.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
+        '''
         if len(s) < 2: return len(s)
         res = 0
         l,r = 0,1
@@ -17,7 +17,17 @@ class Solution:
                 res = max(res, r-l)
 
         return res
-            
+        '''
+        used = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in used:
+                used.remove(s[l])
+                l += 1
+            used.add(s[r])
+            res = max(res, r - l + 1)
+        return res
             
             
          
