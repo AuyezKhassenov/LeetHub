a5baa77e6584f69c96f06dd2104e22dad78c4683
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-02-22 17:43:14 +0600

    Time: 82 ms (62.79%), Space: 14 MB (59.96%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -4,14 +4,14 @@ class Solution:
         
         for i in range(len(s2)):
             if s2[i] in cnt:
-                if cnt[s2[i]] == 0: match -= 1
+                if not cnt[s2[i]]: match -= 1
                 cnt[s2[i]] -= 1
-                if cnt[s2[i]] == 0: match += 1
+                if not cnt[s2[i]]: match += 1
             
             if i >= w and s2[i-w] in cnt:
-                if cnt[s2[i-w]] == 0: match -= 1
+                if not cnt[s2[i-w]]: match -= 1
                 cnt[s2[i-w]] += 1
-                if cnt[s2[i-w]] == 0: match += 1
+                if not cnt[s2[i-w]]: match += 1
             
             if match == len(cnt):
                 return True
