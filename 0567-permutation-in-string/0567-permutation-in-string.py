c779d4e177d571589356f5ff0ecc141835917dcd
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-02-22 17:41:46 +0600

    Time: 103 ms (47.88%), Space: 13.9 MB (59.96%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,21 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        cntr, w, match = Counter(s1), len(s1), 0     
-
+        cnt, match, w = Counter(s1), 0, len(s1)
+        
         for i in range(len(s2)):
-            if s2[i] in cntr:
-                if not cntr[s2[i]]: match -= 1
-                cntr[s2[i]] -= 1
-                if not cntr[s2[i]]: match += 1
-
-            if i >= w and s2[i-w] in cntr:
-                if not cntr[s2[i-w]]: match -= 1
-                cntr[s2[i-w]] += 1
-                if not cntr[s2[i-w]]: match += 1
-
-            if match == len(cntr):
+            if s2[i] in cnt:
+                if cnt[s2[i]] == 0: match -= 1
+                cnt[s2[i]] -= 1
+                if cnt[s2[i]] == 0: match += 1
+            
+            if i >= w and s2[i-w] in cnt:
+                if cnt[s2[i-w]] == 0: match -= 1
+                cnt[s2[i-w]] += 1
+                if cnt[s2[i-w]] == 0: match += 1
+            
+            if match == len(cnt):
                 return True
-
         return False
                 
                 
