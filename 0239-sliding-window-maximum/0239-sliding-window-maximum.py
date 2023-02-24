e30f8adc97d2eb7ec761f4e2baec1a9154c11955
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-02-24 17:34:31 +0600

    Time: 1738 ms (80.85%), Space: 29.9 MB (65.47%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -3,28 +3,24 @@ class Solution:
         #nums = [9,10,9,-7,-4,-8,2,-6]
         #k = 7
         
-        deque = []
+        q = collections.deque()
+        res = []
+        l = r = 0
         
-        for i in range(k):
-            if deque == []:
-                deque.append(nums[i])
-            else:
-                while len(deque) > 0 and nums[i] > deque[-1]:
-                    deque.pop()
-                deque.append(nums[i])      
-        
-        res = [deque[0]]
-        
-        for r in range(k, len(nums)):
-            while len(deque) > 0 and nums[r] > deque[-1]:
-                deque.pop()
+        while r < len(nums):
             
-            if len(deque) and deque[0] == nums[r-k]:
-                deque.pop(0)
-            deque.append(nums[r])    
-            res.append(deque[0])
-            #print(deque)
-                
+            while q and nums[r] > nums[q[-1]]:
+                q.pop()
+            
+            q.append(r)
+            if l > q[0]:
+                q.popleft()
+            
+            if (r+1) >= k:
+                res.append(nums[q[0]])
+                l += 1
+            r += 1
+                            
         return res
             
         
