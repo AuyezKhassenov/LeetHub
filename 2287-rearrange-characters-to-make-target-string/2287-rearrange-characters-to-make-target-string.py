528ae5651185429930a1bb99a1962d53c0000d27
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-03-25 00:54:02 +0600

    Time: 25 ms (94.97%), Space: 13.9 MB (7.55%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -2,7 +2,6 @@ from collections import Counter
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
         count_t = Counter(target)
-        count = [s.count(key)//val for key, val in count_t.items()]
-        return min(count)
+        return min([s.count(key)//val for key, val in count_t.items()])
         
         
