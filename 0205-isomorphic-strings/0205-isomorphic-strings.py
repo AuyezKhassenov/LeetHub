4eeb1882823c55f4a5131d37e471ffdcf046468b
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-03-26 22:40:35 +0600

    Time: 45 ms (48.71%), Space: 16.5 MB (5.28%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,23 +1,5 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        s_dict = defaultdict(int)
-        s_order = defaultdict(int)
-        j = 0
-        for i, ch in enumerate(s):
-            if ch not in s_order:
-                s_order[ch] = j
-                j += 1
-            s_dict[i] = s_order[ch]
-        t_dict = defaultdict(int)
-        t_order = defaultdict(int)
-        j = 0   
-        for i, ch in enumerate(t):
-            if ch not in t_order:
-                t_order[ch] = j
-                j += 1
-            t_dict[i] = t_order[ch]
+        return len(set(list(zip(s,t)))) == len(set(s)) == len(set(t))
         
-        return t_dict == s_dict
-            
-            
         
