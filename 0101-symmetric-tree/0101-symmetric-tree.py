6a36fbac045b33d7ecd653d6d793dbe61bacf400
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-03-27 13:49:54 +0600

    Time: 43 ms (14.63%), Space: 14 MB (48.82%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -18,8 +18,8 @@ class Solution:
                 return False
             
             if left.val == right.val:
-                q.appendleft([left.left, right.right])
-                q.appendleft([left.right, right.left])
+                q.append([left.left, right.right])
+                q.append([left.right, right.left])
             else:
                 return False
         
