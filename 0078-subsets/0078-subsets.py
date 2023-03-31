c1578f8f41aa74cc39bcb138c319f9ba2b3153b0
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-03-31 15:23:43 +0600

    Time: 36 ms (57.13%), Space: 14 MB (77.05%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,8 +1,8 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        
         subset = []
+
         def dfs(i):
             if i >= len(nums):
                 res.append(subset.copy())
@@ -13,9 +13,7 @@ class Solution:
             
             subset.pop()
             dfs(i+1)
-            
         dfs(0)
-        
         return res
                 
                 
