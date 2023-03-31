538e57671a15e0432acc375f44b48d65a28983d8
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-03-31 18:52:31 +0600

    Time: 124 ms (40.94%), Space: 23.3 MB (20.00%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -3,14 +3,14 @@ class Solution:
         res = set()
         
         def dfs(path, tiles):
-            if path:
+            if path not in res:
                 res.add(path)
             
             for i in range(len(tiles)):
                 dfs(path + tiles[i], tiles[:i] + tiles[i+1:])
         
         dfs('', tiles)
-        return len(res)
+        return len(res) - 1
             
                 
             
