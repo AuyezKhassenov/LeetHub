ae1e4406e3cd56d05593704d72f49802ba3b9fb6
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2023-05-19 16:14:49 +0600

    Time: 44 ms (27.92%), Space: 16.4 MB (6.90%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -1,14 +1,13 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
         cnt = defaultdict(set)
-        
-        for i in range(len(rings)//2):
-            cnt[rings[2*i+1]].add(rings[2*i])
-        
+        rings = [rings[i:i+2] for i in range(0,len(rings), 2)]
+        [cnt[key].add(value) for value, key in rings]
         res = 0
-        for u,v in cnt.items():
-            if len(v) == 3:
+        for key,val in cnt.items():
+            if len(val) == 3:
                 res += 1
-        
         return res
+
+
             
