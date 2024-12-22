486c628d02088627415d63e4bc81dbb2df4d6bb4
Cezius <59916141+AuyezKhassenov@users.noreply.github.com>
2024-12-22 22:11:36 +0400

    Time: 7 ms (74.93%), Space: 18.2 MB (10.57%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
@@ -1,30 +1,21 @@
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
-        left2right = [0 for _ in range(len(seats))]
-        right2left = [0 for _ in range(len(seats))]
-        prev = -3e4
-        for i in range(len(seats)):
-            if seats[i] == 1:
-                left2right[i] = i
-                prev = i
-            else:
-                left2right[i] = prev
-                
-        prev = 3e4
-        for i in range(len(seats)-1, -1, -1):
-            if seats[i] == 1:
-                right2left[i] = i
-                prev = i
-            else:
-                right2left[i] = prev
-        print(left2right)
-        print(right2left)
-        ans = 0
-        for i, seat in enumerate(seats):
-            if seat == 0:
-                ans = max(ans, min(i-left2right[i], right2left[i]-i))
-        return ans
-                
+
+        prev, max_len = 0, 0
+
+        for cur, seat in enumerate(seats):
+            if seat:
+                if seats[prev]:
+                    max_len = max(max_len, (cur - prev) // 2)
+                else:
+                    max_len = max(max_len, (cur - prev))
+                prev = cur
+
+        if seats[prev]: 
+            max_len = max(max_len, len(seats) - 1 - prev) 
+
+        return max_len
+
         
                 
         
